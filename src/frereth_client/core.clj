b8d3155122fecf6f0d6345bcf8fa1c1c4b5271ff
James Ashley <james@teamdaves.us>
2013-05-31 22:30:59 -0500

    Thoughts about how this monster should actually work

--- src/frereth_client/core.clj
+++ src/frereth_client/core.clj
@@ -4,9 +4,21 @@
             [frereth-client.config :as config]
             [clojure.tools.nrepl.server :as nrepl]))
 
-(defonce ^:dynamic *renderer-connection* (nrepl/start-server config/*renderer-port*))
+;; Realistically, this belongs in an atom.
+(defonce ^:dynamic *renderer-connection* (atom (nrepl/start-server config/*renderer-port*)))
+
+(defn- kill-renderer-listener
+  "Stop the nrepl listener that's connected to the renderer."
+  ([]
+     (kill-renderer-listener *renderer-connection*))
+  ([connection]
+     (nrepl/stop-server @connection)
+     (swap! @connection (fn [_] nil))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (try
+    (println "Hello, World!")
+    (finally
+      (kill-renderer-listener))))
