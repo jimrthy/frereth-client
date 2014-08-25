90c16feb5b4bd5468b714ef4dd326f0fa88876ef
James Gatannah <jgatannah@gmail.com>
2014-08-25 04:29:53 +0000

    Start thinking through what life looks like w/o JNI

--- src/frereth_client/communicator.clj
+++ src/frereth_client/communicator.clj
@@ -108,6 +108,71 @@ you have bigger issues."
                                          local-server-url])]
     (map->Communicator real-params)))
 
+(comment (defn start [system]
+           (when @(:context system)
+             (throw (RuntimeException. "Trying to restart networking on a system that isn't dead")))
+           (when-let [local-server @(:local-server system)]
+             (throw (RuntimeException. "Trying to start networking when already connected to the local server")))
+           (let [server-connections @(:server-sockets system)]
+             (when (> 0 (count server-connections))
+               (throw (RuntimeException. "Trying to start networking when there are live server connections"))))
+           (when @(:renderer-socket system)
+             (throw (RuntimeException. "Trying to start networking when we still have a live renderer connection")))
+
+           ;; Q: Is there any possible reason to have more than 1 thread here?
+           ;; A: Of course. Should probably make this configurable.
+           ;; Which, realistically, means that there needs to be a way to
+           ;; change this on the fly.
+           ;; Q: How can I tell whether more threads might help?
+           ;; Q: When will it become worth the pain to scrap the existing context and
+           ;; replace all the existing sockets on the fly?
+           (let [ctx (mq/context 1)]
+             (reset! (:context system) ctx)
+
+             ;; Add a socket to a local server
+             ;; FIXME: In all honesty, should do the server handshake, determine the
+             ;; appropriate socket type/url, then connect to that.
+             ;; That's more of a remote server thing, though.
+             ;; TODO: It's still pretty important.
+             (comment (add-server! system ctx (config/local-server-url) :req))
+             ;; Don't worry about remote servers yet
+             (swap! (:local-server system) (fn [_]
+                                             (mq/connected-socket ctx :req (config/local-server-url))))
+
+             ;; Dealer probably isn't the best choice here. Or maybe it is.
+             ;; I want a multi-plexer, but I really want out-going messages to
+             ;; go to everyone connected. So I really need a :pub socket for that,
+             ;; with something like a dealer to handle incoming messages.
+             (let [renderer (mq/connected-socket ctx :dealer (config/render-url))]
+               (reset! (:renderer-socket system) renderer)
+
+               (let [command-channel (async/chan)]
+                 (reset! (:command-channel system) command-channel)
+                 (async/thread-call (fn []
+                                      (translate system)))))
+             system)))
+
+(comment (defn stop [system]
+           (log/trace "Stopping Networking")
+           (when-let [ctx-atom (:context system)]
+             ;; FIXME: Notify renderer that we're going down
+             (when-let [ctx @ctx-atom]
+               (log/info "Disconnecting all servers")
+               (when-let [server-sockets-atom (:server-sockets system)]
+                 (when-let [server-sockets @server-sockets-atom]
+                   (doseq [k (-> system :server-sockets deref)]
+                     (disconnect-server! system k))
+                   (reset! server-sockets-atom {})))
+               (when-let [local-server-atom (:local-server system)]
+                 (when-let [local-server @local-server-atom]
+                   (mq/close! local-server)
+                   (reset! local-server-atom nil)))
+
+               ;; FIXME: Close renderer socket
+
+               (log/trace "Closing messaging context")
+               (mq/terminate! ctx)))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Public
 
@@ -144,70 +209,5 @@ most appropriate to this server. This is really just a minimalist version."
        ;; Do any translation needed.
        (raise :not-implemented)))))
 
-(defn start [system]
-  (when @(:context system)
-    (throw (RuntimeException. "Trying to restart networking on a system that isn't dead")))
-  (when-let [local-server @(:local-server system)]
-    (throw (RuntimeException. "Trying to start networking when already connected to the local server")))
-  (let [server-connections @(:server-sockets system)]
-    (when (> 0 (count server-connections))
-      (throw (RuntimeException. "Trying to start networking when there are live server connections"))))
-  (when @(:renderer-socket system)
-    (throw (RuntimeException. "Trying to start networking when we still have a live renderer connection")))
-
-  ;; Q: Is there any possible reason to have more than 1 thread here?
-  ;; A: Of course. Should probably make this configurable.
-  ;; Which, realistically, means that there needs to be a way to
-  ;; change this on the fly.
-  ;; Q: How can I tell whether more threads might help?
-  ;; Q: When will it become worth the pain to scrap the existing context and
-  ;; replace all the existing sockets on the fly?
-  (let [ctx (mq/context 1)]
-    (reset! (:context system) ctx)
-
-    ;; Add a socket to a local server
-    ;; FIXME: In all honesty, should do the server handshake, determine the
-    ;; appropriate socket type/url, then connect to that.
-    ;; That's more of a remote server thing, though.
-    ;; TODO: It's still pretty important.
-    (comment (add-server! system ctx (config/local-server-url) :req))
-    ;; Don't worry about remote servers yet
-    (swap! (:local-server system) (fn [_]
-                                    (mq/connected-socket ctx :req (config/local-server-url))))
-
-    ;; Dealer probably isn't the best choice here. Or maybe it is.
-    ;; I want a multi-plexer, but I really want out-going messages to
-    ;; go to everyone connected. So I really need a :pub socket for that,
-    ;; with something like a dealer to handle incoming messages.
-    (let [renderer (mq/connected-socket ctx :dealer (config/render-url))]
-      (reset! (:renderer-socket system) renderer)
-
-      (let [command-channel (async/chan)]
-        (reset! (:command-channel system) command-channel)
-        (async/thread-call (fn []
-                             (translate system)))))
-    system))
-
 (defn get-context [system]
   @(:context system))
-
-(defn stop [system]
-  (log/trace "Stopping Networking")
-  (when-let [ctx-atom (:context system)]
-    ;; FIXME: Notify renderer that we're going down
-    (when-let [ctx @ctx-atom]
-      (log/info "Disconnecting all servers")
-      (when-let [server-sockets-atom (:server-sockets system)]
-        (when-let [server-sockets @server-sockets-atom]
-          (doseq [k (-> system :server-sockets deref)]
-            (disconnect-server! system k))
-          (reset! server-sockets-atom {})))
-      (when-let [local-server-atom (:local-server system)]
-        (when-let [local-server @local-server-atom]
-          (mq/close! local-server)
-          (reset! local-server-atom nil)))
-
-      ;; FIXME: Close renderer socket
-
-      (log/trace "Closing messaging context")
-      (mq/terminate! ctx))))
