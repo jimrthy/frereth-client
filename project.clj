c2c16d21a2a8cede28066eecba9e4ce940320966
James Gatannah <jgatannah@gmail.com>
2014-08-25 01:37:40 +0000

    Start looking back over how things are actually supposed to work. My libraries this depends on are totally broken right now, so this won't even compile

--- project.clj
+++ project.clj
@@ -6,10 +6,11 @@
             :url "http://http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[byte-transforms "0.1.3"]
                  [com.postspectacular/rotor "0.1.0"]
+                 [com.stuartsierra/component "0.2.1"]
                  [com.taoensso/timbre "3.2.1"]
                  [im.chit/ribol "0.4.0"]
-                 [jimrthy/cljeromq "0.1.0-SNAPSHOT"]
-                 [org.clojure/tools.logging "0.2.6"]  ;; Q: why am I using this?
+                 [jimrthy/cljeromq "0.1.0-SNAPSHOT"]  ; Q: Do I *really* want to use this?
+                 [org.clojure/tools.logging "0.2.6"]  ; Q: why am I using this?
                  [org.clojure/clojure "1.7.0-alpha1"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
                  ;; Q: What are the odds that this is actually useful?
@@ -21,19 +22,20 @@
                  ;; communicating over nrepl. So...)
                  ;; A: Pretty darn high.
                  [org.clojure/tools.nrepl "0.2.4"]
-                 ;; TODO: Debug profile only
-                 [org.clojure/tools.trace "0.7.8"]]
+                 [prismatic/plumbing "0.3.3"]
+                 [prismatic/schema "0.2.6"]]
   ;;:git-dependencies [["git@github.com:jimrthy/cljeromq.git"]]
   :main frereth-client.core
   ;;:plugins [[lein-git-deps "0.0.1-SNAPSHOT"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[clj-ns-browser "1.3.1"]
                                   [midje "1.6.3"]
-                                  [night-vision "0.1.0-SNAPSHOT"]
                                   [org.clojure/tools.namespace "0.2.5"]
                                   [org.clojure/java.classpath "0.2.2"]
-                                  [ritz/ritz-debugger "0.7.0"]]
-                   :injections [(require 'night-vision.goggles)
-                                (require 'clojure.pprint)]}}
+                                  #_[org.clojure/tools.trace "0.7.8"]
+                                  #_[ritz/ritz-debugger "0.7.0"]]}}
   :repl-options {:init-ns user}
+  ;; Q: What's this needed for?
+  ;; (I think it's core.async, but I'm not sure)
+  ;; TODO: Take it away and see what breaks
   :repositories {"sonatype-nexus-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"})
--- src/frereth_client/communicator.clj
+++ src/frereth_client/communicator.clj
@@ -1,25 +1,115 @@
 (ns frereth-client.communicator
-  (:require [cljeromq.core :as mq]
+  (:require [cljeromq.constants :as mqk]
+            [cljeromq.core :as mq]
             [clojure.core.async :as async]
+            ;; TODO: Honestly, this is bad. It's an implementation
+            ;; detail that I simply do not want to know about
+            [clojure.core.async.impl.channels :refer (ManyToManyChannel)]
             [clojure.tools.logging :as log]
+            [com.stuartsierra.component :as component]
             [frereth-client.config :as config]
-            [ribol.core :refer :all]
+            [plumbing.core :refer :all]
+            [ribol.core :refer (raise)]
+            [schema.core :as s]
             [taoensso.timbre :as timbre])
   (:gen-class))
 
-"Can I handle all of the networking code in here?
-Well, obviously I could. Do I want to?"
+;;;; Can I handle all of the networking code in here?
+;;;; Well, obviously I could. Do I want to?
 
-(defn init []
-  {:context (atom nil)
-   :local-server (atom nil)
-   :server-sockets (atom [])
-   ;; Important implementation note:
-   ;; renderer-socket is bound. Can easily have multiple renderers connected
-   ;; to a single client. Multiplexing out to them becomes an interesting
-   ;; opportunity.
-   :renderer-socket (atom nil)
-   :command-channel (atom nil)})
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Schema
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Utilities
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Lifecycle
+
+(defn build-local-server-connection
+  "For anything resembling an external connection,
+encryption and some sort of auth needs to be mandatory.
+
+If you can't trust communications inside the same process...
+you have bigger issues."
+  [ctx url]
+  ;; This needs to be an async both ways.
+  ;; The other endpoint really should be hard-coded,
+  ;; which makes it very tempting for this to be a
+  ;; router.
+  ;; Or maybe a dealer.
+  ;; Q: Is there an advantage to one of those instead?
+  (-> (mq/socket! ctx (-> mqk/const :socket-type :pair))
+      (mq/connect! url)))
+
+(defn destroy-connection!
+  [sock url]
+  (mq/disconnect! sock url)
+  (mq/close! sock))
+
+(defn build-renderer-binding!
+  ([ctx url]
+     ;; This seems like it should really be a :pair socket instead.
+     ;; Whatever. I have to start somewhere.
+     ;; And this at least introduces the idea that I'm really
+     ;; planning for the client to run on a machine that's as
+     ;; logically separate from the renderer as I can manage.
+     (let [sock (-> mq/socket! ctx (-> mqk/const :socket-type :router))]
+       (mq/bind! sock url)))
+  ([ctx]
+     ;; This approach seems like a horrible failure when
+     ;; it becomes time to shut everything down.
+     ;; Then again, you can't unbind inproc sockets anyway
+     ;; (recognized bug that should be fixed in 4.0.5)
+     (build-renderer-binding! ctx "inproc://local-renderer")))
+
+(defn destroy-renderer-binding
+  [sock url]
+  (mq/unbind! sock url)
+  (mq/close sock))
+
+(defrecord Communicator [command-channel
+                         context
+                         external-server-sockets
+                         local-server
+                         local-server-url
+                         renderer-socket]
+    component/Lifecycle
+
+  (start
+    [this]
+    (let [ctx (mq/context)]
+      (into this {:context ctx
+                  :external-server-sockets (atom {})
+                  :local-server (build-local-server-connection ctx local-server-url)
+                  :renderer-socket (build-renderer-binding ctx)})))
+
+  (stop
+    [this]
+    ;; Q: Do these need to be disconnected first?
+    ;; They can't be bound, since the other side can't possibly know about them
+    (map mq/close! @external-server-sockets)
+    (destroy-renderer-binding renderer-socket)
+    (destroy-connection! local-server local-server-url)
+    (mq/terminate! context)
+    (into this {:context nil
+                :external-server-sockets nil
+                :local-server nil
+                :renderer-socket nil})))
+
+(defn new-communicator
+  [params]
+  ;; I want to use something like s/defn or defnk
+  ;; to be explicit about what I'm getting
+  ;; Q: How do I make that work?
+  #_[command-channel :- ManyToManyChannel
+   local-server :- s/Str]
+  (let [real-params (select-keys params [command-channel
+                                         local-server-url])]
+    (map->Communicator real-params)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Public
 
 (defn add-server!
   "Server handshake really should establish the communication style that's
