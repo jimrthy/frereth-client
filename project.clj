3027ca96a8b5b70087e528cef7c25879016931da
James Gatannah <jgatannah@gmail.com>
2015-06-07 23:28:28 +0000

    Move dependencies into frereth.common

--- project.clj
+++ project.clj
@@ -1,19 +1,10 @@
-
-(defproject frereth-client "0.1.0-SNAPSHOT"
+(defproject com.frereth/client "0.1.0-SNAPSHOT"
   :description "You might think of this as the Web Kit piece, although that's really far too grandiose."
-  :dependencies [[byte-transforms "0.1.4"]
+  :dependencies [[byte-transforms "0.1.4"]  ;; Q: Does this make sense?
+                 [com.frereth/common "0.0.1-SNAPSHOT"]
                  [com.postspectacular/rotor "0.1.0"]
-                 [com.stuartsierra/component "0.2.3"]
-                 [com.taoensso/timbre "3.4.0"]
-                 [im.chit/ribol "0.4.0"]
-                 ;[jimrthy/cljeromq "0.1.0-SNAPSHOT"]  ; Q: Do I *really* want to use this?
                  [org.clojure/tools.logging "0.3.1"]  ; Q: why am I using this?
-                 [org.clojure/clojure "1.7.0-RC1"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
-                 [org.clojure/tools.nrepl "0.2.10"]
-                 [org.zeromq/cljzmq "0.1.4"]
-                 [prismatic/plumbing "0.4.3"]
-                 [prismatic/schema "0.4.2"]]
+                 [org.clojure/tools.nrepl "0.2.10"]]
   :jvm-opts [~(str "-Djava.library.path=/usr/local/lib:" (System/getenv "LD_LIBRARY_PATH"))]
   :license {:name "Eclipse Public License"
             :url "http://http://www.eclipse.org/legal/epl-v10.html"}
@@ -28,7 +19,6 @@
                    :dependencies [[clj-ns-browser "1.3.1"]
                                   [com.cemerick/pomegranate "0.3.0"  ; :exclusions [org.codehaus.plexus/plexus-utils]
                                    ]
-                                  [midje "1.6.3" :exclusions [joda-time]]
                                   [org.clojure/tools.namespace "0.2.10"]
                                   [org.clojure/java.classpath "0.2.2"]]}}
   :repl-options {:init-ns user
