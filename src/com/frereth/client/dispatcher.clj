e14f38460fffe79fcaf94a56c97b03e8afbc7eab
James Gatannah <james.gatannah@gmail.com>
2016-08-20 04:52:16 +0000

    Start getting a handle on message dispatching
    Mainly just starting to define the spec

--- src/com/frereth/client/dispatcher.clj
+++ src/com/frereth/client/dispatcher.clj
@@ -2,33 +2,57 @@
   "For shuffling messages among the server and attached renderers.
 
 This is really only interesting because it needs to support multiple protocols
-and backwards compatibility"
+and backwards compatibility.
+
+Which is YAGNI...for this pass, this really should just be a piece of the
+ConnectionManager."
   (:require [clojure.spec :as s]
             [com.stuartsierra.component :as component]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Specs
 
-(defrecord Dispatcher
-    [render-protocol-version
-     server-protocol-version]
-  component/Lifecycle
-  (start [this]
-    this)
-  (stop [this]
-    this))
-
-(s/def ::major int?)
-(s/def ::minor int?)
-(s/def ::build int?)
-(s/def ::name #{:lolcatz})
-(s/def ::render-protocol-version (s/keys :req [::name ::major ::minor ::build]))
-(s/def ::server-protocol-version (s/keys :req [::name ::major ::minor ::build]))
+(s/def ::render-protocol-version :com.frereth.common.communication/protocol-version)
+(s/def ::server-protocol-version :com.frereth.common.communication/protocol-version)
 
 (s/def :unq/Dispatcher
   (s/keys ::req-un [::render-protocol-version ::server-protocol-version]
           ::opt-un []))
 
+;; For now, at least, this is the SESSION_ID that immutant assigns when a client makes
+;; a web socket connection
+(s/def ::renderer-id string?)
+
+(s/def ::connected-session-atom (fn [x]
+                                  (let [connected-session @x]
+                                    ;; Q: Is this the way it's supposed to work?
+                                    (s/conform ::connected-session connected-session))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Component
+
+;;; One Dispatcher per server connection
+(defrecord Dispatcher
+    ;; this needs an atom of connected renderer sessions.
+    ;; They're all connected to the same Server, by definition.
+    ;; The renderer-id and :world-id should be good enough keys
+    ;; to find the server-side cookie.
+    ;; Probably need to include the renderer-id as an encrypted
+    ;; session cookie so we can find that on the response.
+    ;; Or should I just keep another map of server/world session
+    ;; IDs in here to reduce the risk of other malicious clients
+    ;; from hijacking these sessions?
+    [connected-sessions
+     render-protocol-version
+     server-protocol-version]
+  :load-ns true   ; Q: Is this new?
+  component/Lifecycle
+  (start [this]
+    (let [connected-sessions (or connected-sessions
+                                 (atom {}))])
+    this)
+  (stop [this]
+    this))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Internal
@@ -36,8 +60,19 @@ and backwards compatibility"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Public
 
+(s/fdef renderer->
+        :args (s/cat :this :unq/Dispatcher
+                     :renderer-id ::renderer-id
+                     :world-id :com.frereth.client.world-manager/world-id-type
+                     :msg :com.frereth.common.communication/message))
+(defn renderer->
+  [this renderer-id world-id msg]
+  (let []
+    (throw (ex-info "Not Implemented" {}))))
+
 (s/fdef server->
         :args (s/cat :this :unq/Dispatcher
+                     ;; This is wrong. We're on the dealer side of this socket
                      :msg :com.frereth.common.communication/router-message)
         :ret boolean?)
 (defn server->
