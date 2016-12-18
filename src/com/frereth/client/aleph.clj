6bbfff56c9c9edfcec0150f85427d8007338e1c4
James Gatannah <james.gatannah@gmail.com>
2016-12-18 18:17:10 +0000

    Document why the aleph namespace remains

--- src/com/frereth/client/aleph.clj
+++ src/com/frereth/client/aleph.clj
@@ -1,4 +1,9 @@
 (ns com.frereth.client.aleph
+  "Currently empty and useless, because it's all in common
+
+Leaving it around for a bit, because I don't expect that to
+last, and I don't want to lose the history if I'm correct
+about it."
   (:require [aleph.tcp :as tcp]
             [com.frereth.common.aleph :as common]
             [manifold.deferred :as d]))
