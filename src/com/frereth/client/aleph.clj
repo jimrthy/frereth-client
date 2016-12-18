730379a82db6ad46884d6a31223dfd1869ed9c41
James Gatannah <james.gatannah@gmail.com>
2016-12-18 18:13:57 +0000

    Move the aleph client back into common
    So I have a way to unit test it against the server implementation.
    
    I suppose it would be better to leave them split, but then I'd just
    have to duplicate the other side to write the tests. So it does
    make more sense to leave reference implementations together.

--- src/com/frereth/client/aleph.clj
+++ src/com/frereth/client/aleph.clj
@@ -2,8 +2,3 @@
   (:require [aleph.tcp :as tcp]
             [com.frereth.common.aleph :as common]
             [manifold.deferred :as d]))
-
-(defn client
-  [host port]
-  (d/chain (tcp/client {:host host, :port port})
-           #(common/simplest %)))
